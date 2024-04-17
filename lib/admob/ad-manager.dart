2d463d85657dfe43e8a7375c9c1aa5f2f6b46c47
Fawzi Gharib <fawz11gharib@gmail.com>
2024-04-17 21:02:45 +0300

    FEAT: Ad mob added

---
+++ lib/admob/ad-manager.dart
@@ -0,0 +1,6 @@
+class AdManager {
+  static bool isTest = true;
+  static String bannerHome = isTest
+      ? 'ca-app-pub-3940256099942544/2934735716'
+      : 'ca-app-pub-3940256099942544/2934735716'; // CHANGE THIS ONE TO YOUR AD
+}
